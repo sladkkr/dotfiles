88d6b404b96d18592a780e5f19dfcba16a9f6186
Karol Klimczak <klimczakkarol@outlook.com>
2024-07-01 16:57:46 +0200

    32

--- .config/nvim/lua/personal/environment.lua
+++ .config/nvim/lua/personal/environment.lua
@@ -1,7 +1,9 @@
 vim.cmd'colo kanagawa'
+vim.cmd'set clipboard+=unnamedplus'
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 vim.api.nvim_create_autocmd('VimEnter', { command = 'lcd %:p:h'})
 vim.g.netrw_banner = 0
+
