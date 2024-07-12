7f73ddf0a699fdc54269c9fe754b34bfde0a0a6b
sladkkr <klimczakkarol@outlook.com>
2024-07-12 06:38:59 +0200

    42

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -19,7 +19,7 @@ lsp.setup()
 
 mason.setup({})
 mason_lsp.setup({
-	ensure_installed = {'pyright', 'lua_ls', 'rust_analyzer', 'clangd', 'vls', 'csharp_ls', 'bashls'},
+	ensure_installed = {'lua_ls', 'rust_analyzer', 'clangd', 'vls', 'nil_ls'},
 	handlers = {
 		lsp.default_setup,
 	},
